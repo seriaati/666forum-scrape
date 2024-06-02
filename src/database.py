8f8e27f35604732e586299f8461955b086b915f1
seria <seria.ati@gmail.com>
2024-06-03 07:01:31 +0900

    fix: Fix bug with database

--- src/database.py
+++ src/database.py
@@ -21,10 +21,11 @@ def get_post(posts: list[Post], posted_at: str) -> Post | None:
 
 
 def save_posts(posts_to_save: list[Post], current_posts: list[Post]) -> list[Post]:
-    posts_to_save = [
-        post for post in posts_to_save if get_post(current_posts, post.posted_at) is None
-    ]
+    for post in posts_to_save:
+        if get_post(current_posts, post.posted_at) is not None:
+            continue
+        current_posts.append(post)
     with open(FILE_PATH, "w", encoding="utf-8") as f:
-        json.dump([post.model_dump() for post in posts_to_save], f, ensure_ascii=False, indent=2)
+        json.dump([post.model_dump() for post in current_posts], f, ensure_ascii=False, indent=2)
 
     return posts_to_save
